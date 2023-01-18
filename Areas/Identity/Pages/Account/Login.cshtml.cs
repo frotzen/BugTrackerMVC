071e48f8f4c90c28f54be12d2fb9cf5b603bb150
Elwin Beall <frotzen@gmail.com>
2023-01-18 12:11:10 -0600

    Completed migration after adding Demo Users, updated views

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,14 +21,15 @@ namespace BugTrackerMVC.Areas.Identity.Pages.Account
         private readonly UserManager<BTUser> _userManager;
         private readonly SignInManager<BTUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public LoginModel(SignInManager<BTUser> signInManager, 
-            ILogger<LoginModel> logger,
-            UserManager<BTUser> userManager)
+        public LoginModel(SignInManager<BTUser> signInManager, ILogger<LoginModel> logger,
+            UserManager<BTUser> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            _configuration = configuration; 
         }
 
         [BindProperty]
@@ -72,11 +73,26 @@ namespace BugTrackerMVC.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null, string demoEmail = null)
         {
             returnUrl ??= Url.Content("~/");
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (!string.IsNullOrEmpty(demoEmail))
+            {
+                string? email = _configuration[demoEmail] ?? Environment.GetEnvironmentVariable(demoEmail);
+                string? password = _configuration["DemoUserPassword"] ?? Environment.GetEnvironmentVariable("DemoUserPassword");
+                var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Demo User logged in.");
+                    return LocalRedirect(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid demo login attempt.");
+                    return Page();
+                }
+            }
 
             ModelState.Remove("returnUrl");
             if (ModelState.IsValid)
---
+++ Data/Migrations/20230118173533_Update_add_Demo_Users.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugTrackerMVC.Data.Migrations
+{
+    public partial class Update_add_Demo_Users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
