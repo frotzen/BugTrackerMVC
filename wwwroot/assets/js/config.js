18d113883db67c5634d6b2ac01c9d30088cb46f6
Elwin Beall <frotzen@gmail.com>
2023-01-18 13:05:04 -0600

    Branched AardvarkMVC from Master and removed Xolo template

--- wwwroot/assets/js/config.js
+++ wwwroot/assets/js/config.js
@@ -1,195 +1,27 @@
-var primary = localStorage.getItem("primary") || '#158df7';
-var secondary = localStorage.getItem("secondary") || '#fb2e63';
+/**
+ * Config
+ * -------------------------------------------------------------------------------------
+ * ! IMPORTANT: Make sure you clear the browser local storage In order to see the config changes in the template.
+ * ! To clear local storage: (https://www.leadshook.com/help/how-to-clear-local-storage-in-google-chrome-browser/).
+ */
 
-window.XoloAdminConfig = {
-	// Theme Primary Color
-	primary: primary,
-	// theme secondary color
-	secondary: secondary,
+'use strict';
 
+// JS global variables
+let config = {
+  colors: {
+    primary: '#696cff',
+    secondary: '#8592a3',
+    success: '#71dd37',
+    info: '#03c3ec',
+    warning: '#ffab00',
+    danger: '#ff3e1d',
+    dark: '#233446',
+    black: '#000',
+    white: '#fff',
+    body: '#f4f5fb',
+    headingColor: '#566a7f',
+    axisColor: '#a1acb8',
+    borderColor: '#eceef1'
+  }
 };
-
-// localstorage all setting
-
-$("body").attr("class", localStorage.getItem('body'));
-$("body").attr("class",localStorage.getItem('class'));
-$("html").attr("dir",localStorage.getItem('html'));
-$(".main-nav").attr("class", "main-nav "+localStorage.getItem('main-nav'));
-
-$(".page-wrapper").attr("class", "page-wrapper "+localStorage.getItem('page-wrapper'));
-$(".page-body-wrapper").attr("class", "page-body-wrapper "+localStorage.getItem('page-body-wrapper'));
-
-if (localStorage.getItem("page-wrapper") === null) {
-	$(".page-wrapper").addClass("compact-wrapper");
-	$(".page-body-wrapper").addClass("sidebar-icon");
-}
-
-
-
-// light layout
-$(".default-view").click(function(){
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', ''); 
-
-    // direction html
-    localStorage.setItem('html', 'ltr');
-    localStorage.setItem('class', 'ltr');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-	localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-	
-});
-
-// dark layout
-$("#dark-view").click(function(){
-	localStorage.setItem('body', 'dark-only');
-    localStorage.setItem('main-nav', ''); 
-
-    // direction html
-    localStorage.setItem('html', 'ltr');
-    localStorage.setItem('class', 'ltr');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// compact layout
-$("#compact-view").click(function(){
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', 'close_icon');
-
-    // direction html
-    localStorage.setItem('html', 'ltr');
-    localStorage.setItem('class', 'ltr');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// rtl layout
-$("#rtl-view").click(function(){
-	localStorage.setItem('html', 'rtl'); 
-
-    localStorage.setItem('class', 'rtl'); 
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', '');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// box layout
-$("#box-view").click(function(){
-    // direction html
-    localStorage.setItem('html', 'ltr'); 
-
-    localStorage.setItem('class', 'box-layout'); 
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', '');
-    
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// horizontal layout
-$("#menu-view").click(function(){
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', ''); 
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'horizontal-wrapper');
-    localStorage.setItem('page-body-wrapper', 'horizontal-menu');
-
-    // direction html
-    localStorage.setItem('html', 'ltr');
-    localStorage.setItem('class', 'ltr');
-});
-
-// dark-rtl layout
-$("#darkrtl-view").click(function(){
-    localStorage.setItem('body', 'dark-only');
-    localStorage.setItem('main-nav', ''); 
-
-    // direction html
-    localStorage.setItem('html', 'rtl');
-    localStorage.setItem('class', 'rtl');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// horizontal layout
-$("#verticalrtl-view").click(function(){
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', ''); 
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'horizontal-wrapper');
-    localStorage.setItem('page-body-wrapper', 'horizontal-menu');
-
-    // direction html
-    localStorage.setItem('html', 'rtl');
-    localStorage.setItem('class', 'rtl');
-});
-
-// compact rtl layout
-$("#compactrtl-view").click(function(){
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', 'close_icon');
-
-    // direction html
-    localStorage.setItem('html', 'rtl');
-    localStorage.setItem('class', 'rtl');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// dark box layout
-$("#darkbox-view").click(function(){
-    // direction html
-    localStorage.setItem('html', 'ltr'); 
-
-    localStorage.setItem('class', 'box-layout'); 
-    localStorage.setItem('body', 'dark-only'); 
-    localStorage.setItem('main-nav', '');
-    
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
-
-// horizontal box layout
-$("#verticalbox-view").click(function(){
-    localStorage.setItem('body', 'light-only'); 
-    localStorage.setItem('main-nav', ''); 
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'horizontal-wrapper');
-    localStorage.setItem('page-body-wrapper', 'horizontal-menu');
-
-    // direction html
-    localStorage.setItem('html', 'ltr');
-    localStorage.setItem('class', 'box-layout');
-});
-
-// compact layout
-$("#compactdark-view").click(function(){
-    localStorage.setItem('body', 'dark-only'); 
-    localStorage.setItem('main-nav', 'close_icon');
-
-    // direction html
-    localStorage.setItem('html', 'ltr');
-    localStorage.setItem('class', 'ltr');
-
-    // menu setting
-    localStorage.setItem('page-wrapper', 'compact-wrapper');
-    localStorage.setItem('page-body-wrapper', 'sidebar-icon');
-});
